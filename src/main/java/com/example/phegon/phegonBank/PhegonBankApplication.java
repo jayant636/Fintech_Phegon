f1a06990c2c6dffcf916e92a422e781b2c9a2c73
jayant636 <jayantjalandra@gmail.com>
2026-02-08 18:58:47 +0530

    Basic Skeleton

---
+++ src/main/java/com/example/phegon/phegonBank/PhegonBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.phegon.phegonBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhegonBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhegonBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phegon/phegonBank/PhegonBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phegon.phegonBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhegonBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
